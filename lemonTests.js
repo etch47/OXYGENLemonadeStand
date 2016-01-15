77ba191f3bd22a50990a4dd18755d6ffe911203e
John Fletcher <jbfletcher47@hotmail.com>
2016-01-14 18:14:31 -0600

    working weather and inventory

--- lemonTests.js
+++ lemonTests.js
@@ -26,6 +26,24 @@ function buySupplies(inventory, startingCash){
 
 
 function inventory(buySupplies){
+
+var stockAmount = [];
+for(var ingredient = 0; ingredient< 4; ingredient++){
+    var purchasedStuff = stockAmount[ingredient] + buySupplies[ingredient];
+    
+    console.log("You have  ", purchasedStuff[ingredient], " of ", myStock[ingredient]  )
+    }
+}
+
+
+
+
+
+
+
+
+
+function inventory(buySupplies){
 var myStock = ["cups", "lemons", "sugar", "ice cubes"];
 var stockAmount = [];
 for(var ingredient = 0; ingredient< 4; ingredient++){
@@ -34,3 +52,91 @@ for(var ingredient = 0; ingredient< 4; ingredient++){
     console.log("You have  ", purchasedStuff[ingredient], " of ", myStock[ingredient]  )
     }
 }
+
+
+
+
+
+
+/*
+function inventory(buySupplies){
+var myStock = ["cups", "lemons", "sugar", "ice cubes"];
+var stockAmount = [];
+for(var ingredient = 0; ingredient< 4; ingredient++){
+    var purchasedStuff = stockAmount[index] + buyStuff[index];
+    
+    console.log("You have  ", stockAmount[index], " of ", myStock[]  )
+}
+*/
+
+
+
+
+
+
+
+function buySupplies(){
+    stockAsk = prompt("Do you want want to buy supplies? Y or N?" )
+    if(stockAsk === 'Y' || stockAsk === 'y'){
+        var whatStockAsk = prompt("Which supply do you want to buy: cups, lemons, sugar or ice cubes?");
+        switch(whatStockAsk){
+            case 'cups':
+                cupsAsk = prompt("How many cups would you like to buy? Cups cost " + cupPrice + " cents each.");
+                var buyCups = cupsAsk * cupPrice;
+                console.log("You bought " + cupsAsk + ". Total cost: " + buyCups);
+                break;
+            case 'lemons':
+                lemonsAsk = prompt("How many lemons would you like to buy? Lemons cost " + lemonPrice + " cents each.");
+                console.log("You bought " + lemonsAsk + " lemons.");
+                break;
+            case 'sugar':
+                sugarAsk = prompt("How many cups of sugar would you like to buy? Each cup costs " + sugarPrice + " cents.");
+                console.log("You bought " + sugarAsk + " cups of sugar.");
+                break;
+            case 'ice cubes':
+                iceCubeAsk = prompt("How many ice cubes do you want to buy? Ice cubes cost " + iceCubePrice + " for fifteen cubes.");
+                console.log("You bought " + iceCubeAsk + " ice cubes.");
+               break;
+        }
+    }else if(stockAsk === 'N' || stockAsk === 'n'){
+        console.log("O.K. No supplies are needed");
+    }
+}    
+
+ 
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+   
+   
+function buyMoreCups(buyCups){
+    document.getElementById("buyCupsList").classList.toggle("show");
+    window.onclick = function(event){
+        if (!event.target.matches('.dropbtn')){
+            var dropdowns = document.getElementsByClassName("dropdown-content");
+            var i;
+            for(i = 0; i< dropdowns.length; i++){
+                var openDropdown = dropdowns[i];
+                if (openDropdown.classList.contains('show')){
+                    openDropdown.classList.remove('show');
+                }
+            }
+        }
+    }
+}
+    
+
+
--- newLemon.js
+++ newLemon.js
@@ -1,4 +1,4 @@
-"use strict";
+
 
 var startingCash = 20.00;
 var servingCost = 0.75;
@@ -19,117 +19,105 @@ var inventory = {
 
 var stockPrice = [1,2,3,4];
 
-/*
-function inventory(buySupplies){
-var myStock = ["cups", "lemons", "sugar", "ice cubes"];
-var stockAmount = [];
-for(var ingredient = 0; ingredient< 4; ingredient++){
-    var purchasedStuff = stockAmount[index] + buyStuff[index];
-    
-    console.log("You have  ", stockAmount[index], " of ", myStock[]  )
+
+
+
+
+function buyCups(numberOfCups){
+    inventory.cups += numberOfCups;
+    console.log("You currently have " + numberOfCups + " cups");
 }
-*/
+function buyLemons(numberOfLemons){
+    inventory.lemons += numberOfLemons;
+    console.log("You currently have " + numberOfLemons + " Lemons");
+}
+function buySugar(numberOfSugar){
+    inventory.sugar += numberOfSugar;
+    console.log("You currently have " + numberOfSugar + " cups of sugar");
+}
+function buyIceCubes(numberOfIceCubes){
+    inventory.iceCubes += numberOfIceCubes;
+    console.log("You currently have " + numberOfIceCubes + " Ice Cubes");
+}
 
+buySupplies();
 
-
-
-
-
-
-
-
-
-
- 
-
-function buySupplies(inventory, startingCash){
-    stockAsk = prompt("Do you want want to buy supplies? Y or N?" )
-    if(stockAsk === 'Y' || stockAsk === 'y'){
-        var whatStockAsk = prompt("Which supply do you want to buy: cups, lemons, sugar or ice cubes?");
-        var lowerStockAsk = whatStockAsk.toLowerCase;
-            if(lowerStockAsk === 'cups'){
-                cupsAsk = prompt("How many cups would you like to buy? Cups cost " + cupPrice + " cents each.");
-                var boughtCups = cupsAsk * cupPrice;
-                /* how do I pass this to inventory */
-                console.log("You bought " + cupsAsk + ". Total cost: " + boughtCups);
-            }else if(lowerStockAsk === 'lemons'){
-                lemonsAsk = prompt("How many lemons would you like to buy? Lemons cost " + lemonPrice + " cents each.");
-                var boughtLemons = lemonsAsk * lemonPrice;
-                console.log("You bought " + lemonsAsk + ". Total cost:" + boughtLemons);
-            }else if(lowerStockAsk === 'sugar'){
-                sugarAsk = prompt("How many cups of sugar would you like to buy? Each cup costs " + sugarPrice + " cents.");
-                var boughtSugar = sugarAsk * sugarPrice;
-                console.log("You bought " + sugarAsk + " cups of sugar. Total cost:" + boughtSugar);
-            }else if(lowerStockAsk === 'ice cubes'){
-                iceCubeAsk = prompt("How many ice cubes do you want to buy? Ice cubes cost " + iceCubePrice + " for fifteen cubes.");
-                var boughtIceCubes = iceCubeAsk * iceCubePrice;
-                console.log("You bought " + iceCubeAsk + " ice cubes. Total cost:" + boughtIceCubes);
-    }if(lowerStockAsk === 'N' || stockAsk === 'n'){
-        console.log("O.K. No supplies are needed");
-    }
-}    
-
-
-function inventory(buySupplies){
-var myStock = ["cups", "lemons", "sugar", "ice cubes"];
-var stockAmount = [];
-for(var ingredient = 0; ingredient< 4; ingredient++){
-    var purchasedStuff = stockAmount[ingredient] + buySupplies[ingredient];
-    
-    console.log("You have  ", purchasedStuff[ingredient], " of ", myStock[ingredient]  )
+function doneBuying(){
+    var verifySale;
+    verifySale = prompt("Are you finished buying supplies? Y or N");
+    if(verifySale === 'N' || verifySale === 'n'){
+        buySupplies();
+    }else{
+        console.log("O.K. You are finished buying supplies");        
     }
 }
 
 
 
 
-
-
-/*
 function buySupplies(){
     stockAsk = prompt("Do you want want to buy supplies? Y or N?" )
-    if(stockAsk === 'Y' || stockAsk === 'y'){
-        var whatStockAsk = prompt("Which supply do you want to buy: cups, lemons, sugar or ice cubes?");
-        switch(whatStockAsk){
-            case 'cups':
-                cupsAsk = prompt("How many cups would you like to buy? Cups cost " + cupPrice + " cents each.");
-                var buyCups = cupsAsk * cupPrice;
-                console.log("You bought " + cupsAsk + ". Total cost: " + buyCups);
-                break;
-            case 'lemons':
-                lemonsAsk = prompt("How many lemons would you like to buy? Lemons cost " + lemonPrice + " cents each.");
-                console.log("You bought " + lemonsAsk + " lemons.");
-                break;
-            case 'sugar':
-                sugarAsk = prompt("How many cups of sugar would you like to buy? Each cup costs " + sugarPrice + " cents.");
-                console.log("You bought " + sugarAsk + " cups of sugar.");
-                break;
-            case 'ice cubes':
-                iceCubeAsk = prompt("How many ice cubes do you want to buy? Ice cubes cost " + iceCubePrice + " for fifteen cubes.");
-                console.log("You bought " + iceCubeAsk + " ice cubes.");
-               break;
-        }
-    }else if(stockAsk === 'N' || stockAsk === 'n'){
+    if(stockAsk === 'N' || stockAsk === 'n'){
         console.log("O.K. No supplies are needed");
-    }
-}    
+    }else if(stockAsk === 'Y' || stockAsk === 'y'){
+        var whatStockAsk = prompt("Which supply do you want to buy: cups, lemons, sugar or ice cubes?");
+        if(whatStockAsk === 'cups'){
+            var cupsAsk;
+            cupsAsk = prompt("How many cups would you like to buy? Cups cost " + cupPrice + " cents each.");
+            var boughtCups;
+            boughtCups = cupsAsk * cupPrice;  
+            console.log("You bought " + cupsAsk + " cups. Total cost: " + boughtCups);
+            buyCups(cupsAsk);
+            doneBuying();
+        }else if(whatStockAsk === 'lemons'){
+            var lemonsAsk;
+            lemonsAsk = prompt("How many lemons would you like to buy? Lemons cost " + lemonPrice + " cents each.");
+            var boughtLemons;            
+            boughtLemons = lemonsAsk * lemonPrice;
+            console.log("You bought " + lemonsAsk + " lemons. Total cost:" + boughtLemons);
+            buyLemons(lemonsAsk);
+            doneBuying();
+        }else if(whatStockAsk === 'sugar'){
+            var sugarAsk;
+            sugarAsk = prompt("How many cups of sugar would you like to buy? Each cup costs " + sugarPrice + " cents.");
+            var boughtSugar;
+            boughtSugar = sugarAsk * sugarPrice;
+            console.log("You bought " + sugarAsk + " cups of sugar. Total cost:" + boughtSugar);
+            buySugar(sugarAsk);
+            doneBuying();
+        }else if(whatStockAsk === 'ice cubes'){
+            var iceCubeAsk;
+            iceCubeAsk = prompt("How many ice cubes do you want to buy? Ice cubes cost " + iceCubePrice + " for fifteen cubes.");
+            var boughtIceCubes;
+            boughtIceCubes = iceCubeAsk * iceCubePrice;
+            console.log("You bought " + iceCubeAsk + " ice cubes. Total cost:" + boughtIceCubes);
+            buyIceCubes(iceCubeAsk);
+            doneBuying();   
+        }
+    }    
+}
 
- 
-   
 
 
 
 
+
+
+
+
+
+
+
+var day = ["Monday","Tuesday","Wednesday","Thursday","Friday"];
+var weather = ["Sunny","Cloudy","Raining"];
+var highTemp = 100;
+var lowTemp = 60;
+var tempToday = [];
 
 
 
 
 function makeWeather(){
-    var day = ["Monday","Tuesday","Wednesday","Thursday","Friday"];
-    var weather = ["Sunny","Cloudy","Raining"];
-    var highTemp = 100;
-    var lowTemp = 60;
-    var tempToday = [];
     var todaysForecast;
     var todaysTemp;
     for (var temp = 0; temp < day.length; temp++) {
@@ -141,6 +129,5 @@ function makeWeather(){
 }
 
 makeWeather();
-buySupplies(inventory);
 
-*/
+
